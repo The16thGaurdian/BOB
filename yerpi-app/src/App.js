9e97d35a16ceb5774bbb956e88ddbcac6459a4b7
Huzaifa <thehuzaifasiddiqui@gmail.com>
2024-08-26 22:33:04 -0400

    a

--- yerpi-app/src/App.js
+++ yerpi-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import gear from './gear.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={gear} className="App-logo" alt="logo" />
         <starwars>About Me</starwars>
         <p>
            My Name is Huzaifa Siddiqui
