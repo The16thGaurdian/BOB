c7ab4a90dca057a0294b5fb4af8359948ff65d2d
Huzaifa <thehuzaifasiddiqui@gmail.com>
2024-08-26 22:18:24 -0400

    a

---
+++ yerpi-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <starwars>About Me</starwars>
+        <p>
+           My Name is Huzaifa Siddiqui
+        </p>
+      
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
